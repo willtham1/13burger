b0eaaf2616bfea621539d19837234fd55ea81621
Will Tham <tham.will00@gmail.com>
2022-06-21 14:03:39 -0700

    Deployed

--- config/connection.js
+++ config/connection.js
@@ -11,21 +11,12 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("ERROR connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
 
-if (process.env.JAWSDB_ULR) {
-  connection = mysql.createConnection(process.env.JAWSDB_ULR);
-} else {
-  connection = mysql.createConnection({
-    host: 'z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'sd6s432g20y2sh9u',
-    password: 'bmm3ix4ikmfqlw6s',
-    database: 'tgraeimi3q0hcvpe'
-  });
-};
+
 
 module.exports = connection;
